c2d3cefff6fa2b545d95a20e676f6967ad773791
himanshukumar29 <102627080+himanshukumar29@users.noreply.github.com>
2024-09-01 18:04:50 +0530

    final commit

--- chitra_generator_frontend/src/components/Layout/Footer.js
+++ chitra_generator_frontend/src/components/Layout/Footer.js
@@ -17,10 +17,10 @@ export default function Footer() {
                     <p className='pt-2'>
                         <strong>Connect with me</strong>
                     </p>
-                    <MDBBtn outline color="light" floating className='m-1' href='https://github.com/imvktiwari' role='button'>
+                    <MDBBtn outline color="light" floating className='m-1' href='https://github.com/himanshukumar29' role='button'>
                         <MDBIcon fab icon='github' />
                     </MDBBtn>
-                    <MDBBtn outline color="light" floating className='m-1' href='https://www.instagram.com/imvktiwari/' role='button'>
+                    <MDBBtn outline color="light" floating className='m-1' href='https://www.instagram.com/himanshukumar29/' role='button'>
                         <MDBIcon fab icon='instagram' />
                     </MDBBtn>
                     <MDBBtn outline color="light" floating className='m-1' href='https://www.linkedin.com/in/vaibhav-kumar-tiwari-98a02b238/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BSPdh9buRTBiu4A0ysSzGgg%3D%3D' role='button'>
@@ -33,7 +33,7 @@ export default function Footer() {
 
                 <section className='mb-4'>
                     <p>
-                        I am Vaibhav Kumar Tiwari. I am currently in my third year at the Indian Institute of Technology, Dhanbad, pursuing a Bachelor of Technology in Electronics and Communication Engineering.<br></br>I am a full-stack web developer, and I developed this site as part of my learning journey with OpenAI's APIs.
+                        I am Himanshu Kumar. I am currently in my final year at the Indian Institute of Technology, Dhanbad, pursuing a Bachelor of Technology in Electronics and Communication Engineering.<br></br>I am a full-stack web developer, and I developed this site as part of my learning journey with OpenAI's APIs.
                     </p>
                 </section>
             </MDBContainer>
